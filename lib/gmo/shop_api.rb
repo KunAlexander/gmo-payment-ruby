6196ebd49359656e9342e8d0a8d1feada180f56f
Insight Alexander <alexander.kun@insight-inc.biz>
2022-12-05 21:52:34 +0900

    edit api call method

--- lib/gmo/shop_api.rb
+++ lib/gmo/shop_api.rb
@@ -661,15 +661,15 @@ module GMO
       private
 
         def api_call(name, args = {}, verb = "post", options = {})
-          args.merge!({ "ShopID" => @shop_id, "ShopPass" => @shop_pass })
-          api(name, args, verb, options) do |response|
-            if response.is_a?(Hash) && !response["ErrInfo"].nil?
-              puts 'まだ働いています'
-              puts response["ErrInfo"]
-              puts 'HELLO'
-              raise APIError.new(response, locale)
-            end
-          end
+          # args.merge!({ "ShopID" => @shop_id, "ShopPass" => @shop_pass })
+          # api(name, args, verb, options) do |response|
+          #   if response.is_a?(Hash) && !response["ErrInfo"].nil?
+          #     puts 'まだ働いています'
+          #     puts response["ErrInfo"]
+          #     puts 'HELLO'
+          #     raise APIError.new(response, locale)
+          #   end
+          # end
         end
 
     end
